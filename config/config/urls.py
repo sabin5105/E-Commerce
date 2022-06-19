39800602aec9f3d1c32b24ef7d173aa26430a263
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-06-19 15:37:24 +0900

    Log in configurations

--- config/config/urls.py
+++ config/config/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('register/', views.RegisterFormView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view()),
+    path('product/', views.ProductList.as_view())
 ]
--- config/user/forms.py
+++ config/user/forms.py
@@ -45,4 +45,34 @@ class RegisterForm(forms.Form):
                 )
                 user.save()
 
-    
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        max_length=100,
+        label='Email',
+        error_messages={
+            'required': 'Email is required',
+        }
+    )
+    password = forms.CharField(
+        max_length=50,
+        label='Password',
+        error_messages={
+            'required': 'Password is required',
+        },
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if password and email:
+            try:
+                user = User.objects.get(email=email)
+                if user.password != password:
+                    raise forms.ValidationError('Password does not match')
+                else:
+                    self.email = user.email
+            except User.DoesNotExist:
+                raise forms.ValidationError('User does not exist')
