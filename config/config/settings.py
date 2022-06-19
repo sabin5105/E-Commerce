fc8cdae73640d3dc61021479b5c01d06aa2c78d4
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-06-19 14:01:27 +0900

    connect url

--- config/config/settings.py
+++ config/config/settings.py
@@ -24,7 +24,7 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
